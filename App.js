92f458afcefb151d0efdec8bd2179b73fe236357
Adrian Muñoz <adrianmunoz2005@gmail.com>
2024-05-23 22:57:15 -0300

    practica de styles

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style= {styles.safearea}>
+      <View style= {styles.container}>
+        <Text style={styles.heading }>CUSTOM MOTOS GARAGE </Text>
+      </View>
+
+    </SafeAreaView>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  safearea:{
+    paddingHorizontal:10,
+    paddingVertical:50,
+
   },
-});
+  container: {
+    marginTop: 10,
+    padding: 10,
+    alignItems: 'center',
+  
+  },
+  heading: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    padding:5,
+    borderRadius:5,
+    backgroundColor: "#2aa198",
+    
+    
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'red',
+  },
+})
